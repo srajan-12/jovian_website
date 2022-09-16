f74319021d0c3e1e8ab20dc18bf565be4e6a8068
srajan-12 <66862189+srajan-12@users.noreply.github.com>
2022-09-16 07:47:16 +0000

    added flask project

---
+++ app.py
@@ -0,0 +1,11 @@
+# print(__name__)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Hello, Srajan !!"
+
+if(__name__== '__main__'):
+  app.run(host='0.0.0.0', debug = True)
